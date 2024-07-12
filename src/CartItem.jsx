47eaf0e58cbd68b03547fbc02b1a02bebc2e826a
Pedro <xpedroes@gmail.com>
2024-07-12 01:17:23 -0600

    finished app

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -77,6 +77,7 @@ const Cart = ({onContinueShopping}) => {
   );
 };
 
+
 export default Cart;
 
 
--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -4,6 +4,7 @@ export const CreatSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalItems: 0
   },
   reducers: {
     addItem: (state, action) => {
@@ -14,20 +15,29 @@ export const CreatSlice = createSlice({
     } else {
       state.items.push({ name, image, cost, quantity: 1 });
     }
+    state.totalItems++;
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter(item => item.name !== action.payload.name);
+      const { name } = action.payload;
+      const itemToRemove = state.items.find(item => item.name === name);
+
+      if (itemToRemove) {
+        state.totalItems -= itemToRemove.quantity;  // Actualiza el número total de ítems
+        state.items = state.items.filter(item => item.name !== name);
+      }
     },
     updateQuantity: (state, action) => {
       const {name, quantity} = action.payload;
       const itemToUpdate = state.items.find(item => item.name === name);
       if (itemToUpdate){
+        state.totalItems += (quantity - itemToUpdate.quantity);
         itemToUpdate.quantity = quantity;
       }
     },
   },
 });
 
+export const selectTotalItems = state => state.cart.totalItems;
 
 export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
 
